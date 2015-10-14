bc3c0c0e1d2c391f85dc725f214aabf8da3d8906
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2015-10-15 01:20:00 +0200

    Fix issue on imag forge (and fix quotation)

--- forges/imag.sh
+++ forges/imag.sh
@@ -7,11 +7,14 @@ declare_forge \
   --install-hooks imag_install_hooks
 
 imag_install_hooks() {
+	local prefix=/home/users/vdanjean
+	local bindir="$prefix/bin"
+	local libdir="$prefix/lib"
 	echo "WARNING: This forge misses some basic binaries (mkdir, ...)"
-	echo "WARNING: adding /home/users/vdanjean/bin in remote PATH"
-	echo "WARNING: adding /home/users/vdanjean/lib in remote LD_LIBRARY_PATH"
+	echo "WARNING: adding $bindir in remote PATH"
+	echo "WARNING: adding $libdir in remote LD_LIBRARY_PATH"
 	echo 
-	SSH_CMD=( "${SSH_CMD[@]}" 'PATH="$HOME/bin:$PATH"' 'LD_LIBRARY_PATH="$HOME/lib"' )
+	SSH_CMD=( "${SSH_CMD[@]}" "$bindir/env" 'PATH="'"$bindir"':$PATH"' 'LD_LIBRARY_PATH="'"$libdir"'"' )
 	# la forge imag ne contient pas les templates de git :-(
 	CREATE_LOCAL=1
 }
